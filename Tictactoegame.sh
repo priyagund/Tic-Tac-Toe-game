cb2986380566dd9ec8d2189c34fcb7f007dfd93e
priyagund <priyagund18@gmail.com>
2019-11-16 19:26:49 +0530

    resetting the board

--- Tictactoegame.sh
+++ Tictactoegame.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "welcome"
+
+declare -a  boardOfGame
+function resetingBoard()
+{
+ 	for((position=1 ; position<=9 ; position++ ))
+	do
+	boardOfGame["$position"]=$position;
+        
+	done
+echo :${boardOfGame[@]}
+
+
+}
+resetingBoard
