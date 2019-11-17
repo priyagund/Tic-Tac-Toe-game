88d4867c7f49ab17cc747a9327df33a0588f6bf8
priyagund <priyagund18@gmail.com>
2019-11-17 12:49:58 +0530

    display board for playing

--- Tictactoegame.sh
+++ Tictactoegame.sh
@@ -4,19 +4,21 @@ echo "welcome"
 declare -a  boardOfGame
 function resetingBoard()
 {
+	
  	for((position=1 ; position<=9 ; position++ ))
 	do
 	boardOfGame["$position"]=$position;
         
 	done
-echo :${boardOfGame[@]}
+	displayBoard
 
+     
 
 }
 
 function assignedSymbol() 
 {
-	resetingBoard
+	
 	read -p "take lettrer (x or 0) for playing:" playerLetter 
 	if [ $playerLetter == x ] || [ $playerLetter == X ]
 	then 
@@ -26,7 +28,7 @@ function assignedSymbol()
 	fi
 	}
  	
-	assignedSymbol   
+	 
 
 function tossWinner() 
 {	
@@ -41,4 +43,19 @@ function tossWinner()
 
 }
 
+
+
+function displayBoard()
+{	
+	
+	echo "  |---|---|---| "
+	echo "  | "${boardOfGame[1]}" | "${boardOfGame[2]}" | "${boardOfGame[3]}" |"
+	echo "  |---|---|---|"
+	echo "  | "${boardOfGame[4]}" | "${boardOfGame[5]}" | "${boardOfGame[6]}" |"
+	echo "  |---|---|---|"		
+	echo "  | "${boardOfGame[7]}" | "${boardOfGame[8]}" | "${boardOfGame[9]}" |"       
+        echo "  |---|---|---|"
+}
 tossWinner
+resetingBoard
+
