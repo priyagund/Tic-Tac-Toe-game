4d1ee127a2c637bf0cae6d1b56784e4887769297
priyagund <priyagund18@gmail.com>
2019-11-20 11:36:04 +0530

    ADD:check player can win give chance to computer

--- Tictactoegame.sh
+++ Tictactoegame.sh
@@ -31,12 +31,12 @@ function tossWinner()
 	tossCheck=$((RANDOM%2))
 	if [ $tossCheck == 1 ]
 	then 
-	tossCheck1=$pLetter
+	tossCheck1=$playerLetter
 	else
-	tossCheck1=$comLetter
+	tossCheck1=$computerLetter
 	fi
         
-        if [ $tossCheck1 == $pLetter ]
+        if [ $tossCheck1 == $playerLetter ]
         then
 		displayBoard 
 		playGame $tossCheck1 $playerLetter $computerLetter
@@ -62,105 +62,237 @@ function displayBoard()
 
 function winAtRowsPosition() 
 {
-	
-	i=0
-	letter=$1
-	resultRow=nextmove
-	for((count=1; count<=3 ; count++ ))
+	winnerFound=false;
+	local rowCount=0
+	for (( count=1; count<=3; count++ ))
 	do
-	i=$(($i+1))
-	if [[ ${boardOfGame[$i]} -eq ${boardOfGame[$i +1]} ]] && [[ ${boardOfGame[$i]} -eq ${boardOfGame[$i + 2]} ]] && [[ ${boardOfGame[$i +2]} -eq $letter ]]
-	then
-	resultRow="win"
-        break
-	
-	fi
-	i=$(($i+2))
+		rowCount=$(( $rowCount+1 ))
+		if [[ ${boardOfGame[$rowCount]} == ${boardOfGame[$rowCount+1]} ]] && [[ ${boardOfGame[$rowCount+1]} == ${boardOfGame[$rowCount+2]} ]]
+		then
+			winnerFound=true;
+			break;
+		else
+			rowCount=$(( $rowCount+2 ))
+		fi
 	done
-	echo $resultRow
+	echo $winnerFound
 }
+
+
 
 function winAtColumnPosition() 
 {
-	
-	letter=$1
-	resultColumn=nextmove
-	for (( i=1; i<=3 ; i++ ))
+	local columCount=0;
+	local winnerFound=false;
+	for (( count=1; count<=3; count++ ))
 	do
-	
-	if [ ${boardOfGame[$i]} == ${boardOfGame[$((i +3))]} ] && [ ${boardOfGame[$i]} == ${boardOfGame[$((i+6))]} ] && [ ${boardOfGame[$((i +6))]} == $letter ]
-	then
-	resultColumn="win"
-	break
-	fi
-	
-	
+		columCount=$(( $columCount+1 ))
+		if [[ ${boardOfGame[$columCount]} == ${boardOfGame[$columCount+3]} ]] && [[ ${boardOfGame[$columCount+3]} == ${boardOfGame[$columCount+6]} ]]
+		then
+			winnerFound=true;
+			break;
+		fi
 	done
-	echo $resultColumn
+	echo $winnerFound
+
 }
 
-function winAtDiagonal() 
+function winAtDiagonalPosition() 
 {
 	
-	letter=$1
-	resultDiagonal=nextmove
-	i=1
-	if [ ${boardOfGame[$i]} == ${boardOfGame[$((i +4))]} ] && [ ${boardOfGame[$i]} == ${boardOfGame[$((i +8))]} ] && [ ${boardOfGame[$((i +8))]} == $letter ]
-	then
-	resultDiagonal="win"
-	
-	elif [ ${boardOfGame[$((i +2))]} == ${boardOfGame[$((i +4))]} ] && [ ${boardOfGame[$((i +2))]} == ${boardOfGame[$((i +6))]} ] && [ ${boardOfGame[$((i +6))]} == $letter ]
-	then
-	resultDiagonal="win"
-	
-	
-	fi
+	local winnerFound=false
+	for (( count=1; count<=3; count++ ))
+	do
+		if [[ ${boardOfGame[$count]} == ${boardOfGame[$count+4]} ]] && [[ ${boardOfGame[$count+4]} == ${boardOfGame[$count+8]} ]]
+		then
+			winnerFound=true;
+			break;
+		elif [[ ${boardOfGame[$count+2]} == ${boardOfGame[$count+4]} ]] && [[ ${boardOfGame[$count+4]} == ${boardOfGame[$count+6]} ]]
+		then
+			winnerFound=true;
+			break;
+		fi
+	done
+	echo $winnerFound;
 	
 }
 
 
-function playGame() 
-{	turn=$2
-	playNext=nextmove
-	
-	while [[ $playNext == nextmove ]]
+function playGame() {
+	playNext=0
+	turn=$2
+	computerPlay=play
+	playerPlay=play
+	winnerFound1=false
+	while [ $playNext == 0 ]
 	do
-		read -p "enter the cell:" position
-		echo $position
- 	if [[ ${boardOfGame[$position]} == $2 ]] || [[ ${boardOfGame[$position]} == $3 ]]
-	then
-		playNext=nextmove
-	else
-		boardOfGame[$position]=$2
-		displayBoard
-		playNext="$( checkWinTieOrTurnChange $1 $2 $3 )"
+		playerPlay="play"
+		while [ $playerPlay == play ]
+		do
+			read -p "Enter the Cell Number" position
+			boardOfGame[$position]=$2 
+			displayBoard
+			winnerFound="$( checkWinTieOrTurnChange $2 )"
+			playerPlay=stop				
+			if [ $winnerFound == true ]
+			then
+				echo "PLAYER WON"
+				break
+			fi
 		
-	fi
-	if [ $playNext == win ]
-	then
-		echo "win Player"
-		break
-	fi
+		done
+		if [ $winnerFound == true ]
+		then
+			break
+		fi
+		computerPlay="play"
+		while [ $computerPlay == play ] 
+		do
+			computerPosition="$( possiblityForWinning $1 $2 $3 )"
+			winnerFound1=false
+			if [ ${boardOfGame[$computerPosition]} == $computerPosition ]
+			then
+				boardOfGame[$computerPosition]=$3
+				displayBoard
+				winnerFound1="$( checkWinTieOrTurnChange $3 )"
+				computerPlay=stop
+			fi
+			if [ $winnerFound1 == true ]
+			then
+				echo "COMPUTER WON"
+				break
+			fi
+		done  
+		echo "WinnerFound==================>"$winnerFound1	
+		if [ $winnerFound1 == true ]
+		then
+			break
+		fi
 	done
- 
+
+}
+
+function possiblityForWinning(){
+	rowPosition="$( winnerAtRow $1 $2 $3)"
+	columnPosition="$( winnerAtColumn $1 $2 $3 )"
+	diagonalPosition="$( winnerAtDiagonal $1 $2 $3 )"
+	
+	if [[ $rowPosition -gt 0 ]]
+	then
+		randomPoition=$rowPosition
+		replacePosition=0;
+	elif [[ $columnPosition -gt 0 ]]
+	then
+
+		randomPoition=$columnPosition
+		replacePosition=0;
+	elif [[ $diagonalPosition -gt 0 ]]
+	then
+
+		randomPoition=$diagonalPosition
+		replacePosition=0;
+	else
+		randomPoition=$((RANDOM%9+1))	
+	fi
+	echo $randomPoition
 }
 
  function checkWinTieOrTurnChange() 
-{	resultTurn=$1
-	rowResult="$(winAtRowsPosition $2)"
-	columnResult="$(winAtColumnPosition $2)"
-	diagonalResult="$(winAtDiagonal $2)"
-	if [[ $rowResult == "win" ]] || [[ $columnResult == "win" ]] || [[ $diagonalsResult == "win" ]]
+{	rowResult="$(winAtRowsPosition $1 $2 $3)"
+	columnResult="$(winAtColumnPosition $1 $2 $3)"
+	diagonalsResult="$(winAtDiagonalPosition $1 $2 $3)"
+	flag=false
+	if [[ $rowResult == true ]] || [[ $columnResult == true ]] || [[ $diagonalsResult == true ]]
 	then
-		flag="win"
-	else
-		resultOfTie="$(tieResult)"		
-		if [ $resultOfTie == nextmove ]
-		then
-			flag=nextmove
-		fi
+		flag=true
+	#else
+		#resultOfTie="$(tieResult)"		
+		#if [ $resultOfTie == nextmove ]
+		#then
+			#flag=false
+		#else
+			#flag=false
+		#fi
 	fi
 	echo $flag
+}
+
+
+function winnerAtRow(){
+	local computerLetter=$3
+	local playerLetter=$2
+	local row=1;
+	local winnerFound=false;
+	for (( count=1; count<=3; count++ ))
+	do
+		
+		if [[ ${boardOfGame[$row]} == ${boardOfGame[$row+1]} ]] || [[ ${boardOfGame[$row+1]} == ${boardOfGame[$row+2]} ]] || [[ ${boardOfGame[$row+2]} == ${boardOfGame[$row]} ]]
+		then
+			for (( counter=$row; counter<=$row+2; counter++ ))
+			do
+				if [[ ${boardOfGame[$counter]} -ne $computerLetter ]] 
+				then
+					replacePosition=$counter
+				fi
+			done
+		else
+			row=$(( $row+1 ))
+		fi
+	done
+	echo $replacePosition
+}
+
+function winnerAtColumn(){
+	local playerLetter=$2
+	local computerLetter=$3
+	local column=1;
+	local winnerFound=false;
+	for (( count=1; count<=3; count++ ))
+	do
+		
+		if [[ ${boardOfGame[$column]} == ${boardOfGame[$column+3]} ]] || [[ ${boardOfGame[$column+3]} == ${boardOfGame[$column+6]} ]] || [[ ${boardOfGame[$column+6]} == ${boardOfGame[$column]} ]] 
+		then
+			for (( counter=1; counter<=3 ;counter++ ))
+			do
+				if [[ ${boardOfGame[$column]} -ne $computerLetter ]] 
+				then
+					replacePosition=$counter
+				fi
+				column=$(( $column+3 ))
+			done
+		fi
+	done
+	echo $replacePosition
+}
+
+function winnerAtDiagonal() {
+	local playerLetter=$2
+	local computerLetter=$3
+	#local diagCount=1;
+	local count=1;
+	local winnerFound=false;
+	if [[ ${boardOfGame[$count]} == ${boardOfGame[$count+4]} ]] || [[ ${boardOfGame[$count+4]} == ${boardOfGame[$count+8]} ]] || [[ ${boardOfGame[$count+8]} == ${boardOfGame[$count]} ]]
+	then
+		for (( counter=1; counter<=3; counter++ ))
+		do
+			if [[ ${boardOfGame[$count]} -ne $computerLetter ]]
+			then
+				replacePosition=$count
+			fi
+			count=$(( $count+4 ))
+		done
+	elif [[ ${boardOfGame[$count+2]} == ${boardOfGame[$count+4]} ]] || [[ ${boardOfGame[$count+4]} == ${boardOfGame[$count+6]} ]] || [[ ${boardOfGame[$count+6]} == ${boardOfGame[$count+2]} ]]
+	then
+		for (( counter=1; counter<=3; counter++ ))
+		do
+			count=$(( $count+2 ))
+			if [[ ${boardOfGame[$count]} -ne $computerLetter ]]
+			then
+				replacePosition=$count
+			fi
+		done
+	fi
+	echo $replacePosition
 }
 
  function tieResult() 
@@ -177,6 +309,3 @@ function playGame()
 
 
 tossWinner
-
-
- 
